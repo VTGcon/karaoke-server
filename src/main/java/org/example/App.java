41dbed024aac04c92e0426db85e7812cc07bde8e
VTGcon <vyacheslav.shabes@yandex.ru>
2022-06-28 13:40:18 +0300

    hehe

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,10 +12,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    static void createServer() throws Exception {
         Server server = ServerBuilder.forPort(50051).addService(new GreetingServiceImpl()).build();
         server.start();
         System.out.println("ABOBA!");
         server.awaitTermination();
+    }
+
+    static void run() {
+        try {
+            createServer();
+        } catch (Exception e) {
+            System.out.println("ABOBA ended(");
+            e.printStackTrace();
+            run();
+        }
+    }
+
+    public static void main(String[] args) {
+        run();
     }
 }
